e3df0d77c830061cdf579a7240f8e419ff0a0429
Ghayas <sheikhghayas47@gmail.com>
2022-09-11 13:19:41 +0500

    **Error** - Bitmap Immutable issue resolved.
    **Features Added**
    - Trim empty part of bitmap feature added

--- app/src/main/java/com/example/backgroundremoverlibrary/MainActivity.kt
+++ app/src/main/java/com/example/backgroundremoverlibrary/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.example.backgroundremoverlibrary
 
-import android.content.res.Resources
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
 import com.example.backgroundremoverlibrary.databinding.ActivityMainBinding
 import com.slowmac.autobackgroundremover.BackgroundRemover
 import com.slowmac.autobackgroundremover.OnBackgroundChangeListener
-import java.lang.Exception
-import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val displayWidth = Resources.getSystem().displayMetrics.widthPixels
+    private val imageResult =
+        registerForActivityResult(
+            ActivityResultContracts.GetContent()
+        ) { uri: Uri? ->
+            uri?.let { uri ->
+                binding.img.setImageURI(uri)
+            }
+        }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,19 +29,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        imageResult.launch("image/*")
 
-        val bitmap = getBitmapFromRes(resources, R.drawable.woman, displayWidth)
-        BackgroundRemover.bitmapForProcessing(bitmap, object: OnBackgroundChangeListener{
-            override fun onSuccess(bitmap: Bitmap) {
-                binding.img.setImageBitmap(bitmap)
-            }
 
-            override fun onFailed(exception: Exception) {
-                Toast.makeText(this@MainActivity, "Error Occur", Toast.LENGTH_SHORT).show()
-            }
 
-        })
+        binding.removeBgBtn.setOnClickListener {
+            removeBg()
+        }
 
+
+    }
+
+
+    private fun removeBg() {
+        binding.img.invalidate()
+        BackgroundRemover.bitmapForProcessing(
+            binding.img.drawable.toBitmap(),
+            true,
+            object : OnBackgroundChangeListener {
+                override fun onSuccess(bitmap: Bitmap) {
+                    binding.img.setImageBitmap(bitmap)
+                }
+
+                override fun onFailed(exception: Exception) {
+                    Toast.makeText(this@MainActivity, "Error Occur", Toast.LENGTH_SHORT).show()
+                }
+
+            })
     }
 
 
--- autobackgroundremover/src/main/java/com/slowmac/autobackgroundremover/BackgroundRemover.kt
+++ autobackgroundremover/src/main/java/com/slowmac/autobackgroundremover/BackgroundRemover.kt
@@ -1,12 +1,16 @@
 package com.slowmac.autobackgroundremover
 
 import android.graphics.Bitmap
+import android.graphics.Color
+import android.util.Log
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.segmentation.Segmentation
 import com.google.mlkit.vision.segmentation.Segmenter
 import com.google.mlkit.vision.segmentation.selfie.SelfieSegmenterOptions
 import kotlinx.coroutines.*
 import java.nio.ByteBuffer
+import kotlin.system.measureTimeMillis
+
 
 object BackgroundRemover {
 
@@ -18,18 +22,26 @@ object BackgroundRemover {
 
     init {
         val segmentOptions = SelfieSegmenterOptions.Builder()
-                .setDetectorMode(SelfieSegmenterOptions.SINGLE_IMAGE_MODE)
-                .build()
-
+            .setDetectorMode(SelfieSegmenterOptions.SINGLE_IMAGE_MODE)
+            .build()
         segment = Segmentation.getClient(segmentOptions)
     }
 
 
     /**
      * Process the image to get buffer and image height and width
-     * */
-    fun bitmapForProcessing(image: Bitmap, listener: OnBackgroundChangeListener) {
-        val input = InputImage.fromBitmap(image, 0)
+     * @param bitmap Bitmap which you want to remove background.
+     * @param trimEmptyPart After removing the background if its true it will remove the empty part of bitmap. by default its false.
+     * @param listener listener for success and failure callback.
+     **/
+    fun bitmapForProcessing(
+        bitmap: Bitmap,
+        trimEmptyPart: Boolean? = false,
+        listener: OnBackgroundChangeListener
+    ) {
+        //Generate a copy of bitmap just in case the if the bitmap is immutable.
+        val copyBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val input = InputImage.fromBitmap(copyBitmap, 0)
         segment.process(input)
             .addOnSuccessListener { segmentationMask ->
                 buffer = segmentationMask.buffer
@@ -37,23 +49,35 @@ object BackgroundRemover {
                 height = segmentationMask.height
 
                 CoroutineScope(Dispatchers.IO).launch {
-                    val bitmap = removeBackgroundFromImage(image)
-                    withContext(Dispatchers.Main){
-                        listener.onSuccess(bitmap)
+                    val time = measureTimeMillis {
+                        val resultBitmap = if (trimEmptyPart == true) {
+                            val bgRemovedBitmap = removeBackgroundFromImage(copyBitmap)
+                            trim(bgRemovedBitmap)
+                        } else {
+                            removeBackgroundFromImage(copyBitmap)
+                        }
+                        withContext(Dispatchers.Main) {
+                            listener.onSuccess(resultBitmap)
+                        }
                     }
+                    Log.e("TAG", "bitmapForProcessingTime: $time")
                 }
 
             }
             .addOnFailureListener { e ->
                 println("Image processing failed: $e")
                 listener.onFailed(e)
+
             }
     }
 
+
     /**
-     * Remove the background pixels from the image
+     * Change the background pixels color to transparent.
      * */
-    private suspend fun removeBackgroundFromImage(image: Bitmap): Bitmap {
+    private suspend fun removeBackgroundFromImage(
+        image: Bitmap
+    ): Bitmap {
         val bitmap = CoroutineScope(Dispatchers.IO).async {
             for (y in 0 until height) {
                 for (x in 0 until width) {
@@ -69,4 +93,54 @@ object BackgroundRemover {
         return bitmap.await()
     }
 
+
+    /**
+     * trim the empty part of a bitmap.
+     **/
+    private suspend fun trim(
+        bitmap: Bitmap
+    ): Bitmap {
+        val result = CoroutineScope(Dispatchers.Default).async {
+            var firstX = 0
+            var firstY = 0
+            var lastX = bitmap.width
+            var lastY = bitmap.height
+            val pixels = IntArray(bitmap.width * bitmap.height)
+            bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+            loop@ for (x in 0 until bitmap.width) {
+                for (y in 0 until bitmap.height) {
+                    if (pixels[x + y * bitmap.width] != Color.TRANSPARENT) {
+                        firstX = x
+                        break@loop
+                    }
+                }
+            }
+            loop@ for (y in 0 until bitmap.height) {
+                for (x in firstX until bitmap.width) {
+                    if (pixels[x + y * bitmap.width] != Color.TRANSPARENT) {
+                        firstY = y
+                        break@loop
+                    }
+                }
+            }
+            loop@ for (x in bitmap.width - 1 downTo firstX) {
+                for (y in bitmap.height - 1 downTo firstY) {
+                    if (pixels[x + y * bitmap.width] != Color.TRANSPARENT) {
+                        lastX = x
+                        break@loop
+                    }
+                }
+            }
+            loop@ for (y in bitmap.height - 1 downTo firstY) {
+                for (x in bitmap.width - 1 downTo firstX) {
+                    if (pixels[x + y * bitmap.width] != Color.TRANSPARENT) {
+                        lastY = y
+                        break@loop
+                    }
+                }
+            }
+            return@async Bitmap.createBitmap(bitmap, firstX, firstY, lastX - firstX, lastY - firstY)
+        }
+        return result.await()
+    }
 }
